4db0043ddf433c6349b3b4262a9e0e5f1d0df0a0
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-06-18 15:55:57 +0530

    ADDED:To check distance time return total fare

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -1,7 +1,19 @@
 package com.bridgelabz;
 
 public class InvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("Welcome Cab Invoice Generator ");
+    private int COST_PER_KILOMETER = 10;
+    private int COST_PER_MINUTE = 1;
+    private int MINIMUM_COST = 5;
+    double distance;
+    double time;
+
+    public InvoiceGenerator(double distance, double time) {
+        this.distance = distance;
+        this.time = time;
+    }
+
+    public double getTotalFare() {
+        double totalFare = distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
+        return totalFare;
     }
 }
--- src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
@@ -1,5 +1,13 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 public class InvoiceGeneratorTest {
-    InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+    @Test
+    public void givenDistanceTime_ReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator(10, 10);
+        double totalFare = invoiceGenerator.getTotalFare();
+        Assert.assertEquals(110, totalFare, 0);
+    }
 }
