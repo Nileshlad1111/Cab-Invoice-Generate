dbde39f82fbc6920a359c96ab43c4c974ff51aa8
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-06-18 16:22:20 +0530

    ADDED:To check given less distance time and calculate total fare

--- src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
@@ -10,4 +10,11 @@ public class InvoiceGeneratorTest {
         double totalFare = invoiceGenerator.getTotalFare();
         Assert.assertEquals(110, totalFare, 0);
     }
+
+    @Test
+    public void givenLessDistanceTime_ReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator(0.1, 1);
+        double totalFare = invoiceGenerator.getTotalFare();
+        Assert.assertEquals(5, totalFare, 0);
+    }
 }
