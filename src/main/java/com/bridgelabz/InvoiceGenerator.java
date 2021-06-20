a106e32df933b06d9db7dae342cdb4a0ec5bb384
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-06-20 16:56:44 +0530

    ADDED : Test Case, Code And Ride Class For Multiple Rides Feature

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -1,19 +1,36 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+
 public class InvoiceGenerator {
     private int COST_PER_KILOMETER = 10;
     private int COST_PER_MINUTE = 1;
     private int MINIMUM_COST = 5;
-    double distance;
-    double time;
+    private double distance;
+    private double time;
+    private double totalFare;
 
     public InvoiceGenerator(double distance, double time) {
         this.distance = distance;
         this.time = time;
     }
 
+    public InvoiceGenerator() {
+
+    }
+
     public double getTotalFare() {
         double totalFare = distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
+        return (totalFare > MINIMUM_COST) ? totalFare : MINIMUM_COST;
+    }
+
+    public double getTotalFare(ArrayList<Ride> rides) {
+        for (Ride ride :
+                rides) {
+            this.distance = ride.distance;
+            this.time = ride.time;
+            this.totalFare += getTotalFare();
+        }
         return totalFare;
     }
 }
--- src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
@@ -3,6 +3,9 @@ package com.bridgelabz;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class InvoiceGeneratorTest {
     @Test
     public void givenDistanceTime_ReturnTotalFare() {
@@ -17,4 +20,14 @@ public class InvoiceGeneratorTest {
         double totalFare = invoiceGenerator.getTotalFare();
         Assert.assertEquals(5, totalFare, 0);
     }
+
+    @Test
+    public void givenDistanceTimeForRides_ReturnInvoiceSummary() {
+        ArrayList<Ride> rides = new ArrayList<>(Arrays.asList(new Ride(12, 10)
+                , new Ride(6, 12), new Ride(24, 12)));
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double totalFare = invoiceGenerator.getTotalFare(rides);
+        Assert.assertEquals(454, totalFare, 0);
+    }
+
 }
